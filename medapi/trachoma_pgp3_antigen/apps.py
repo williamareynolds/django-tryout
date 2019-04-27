f1c78c6ee940548ef5926f4188fcd90786a866f9
William Reynolds <williamareynolds@icloud.com>
2019-04-27 11:36:38 -0500

    initial

---
+++ medapi/trachoma_pgp3_antigen/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class TrachomaPgp3AntigenConfig(AppConfig):
+    name = 'trachoma_pgp3_antigen'
